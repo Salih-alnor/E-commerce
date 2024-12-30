f5fcb269b55ae6e54ce0a0e36514b46a65d31742
Salih-alnor <salih.gad@icloud.com>
2024-12-30 15:04:49 +0300

    update profile screen

--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -6,6 +6,8 @@ import {
   Dimensions,
   TouchableOpacity,
   ScrollView,
+  Modal,
+  Alert,
 } from "react-native";
 import React, { useState, useEffect } from "react";
 import COLORS from "../assets/colors";
@@ -15,6 +17,9 @@ import mail from "../assets/images/icons/mail.png";
 import share from "../assets/images/icons/share.png";
 import help from "../assets/images/icons/help.png";
 import next from "../assets/images/icons/next.png";
+import remove from "../assets/images/icons/remove.png";
+import camera from "../assets/images/icons/camera.png";
+import gallery from "../assets/images/icons/gallery.png";
 import * as ImagePicker from "expo-image-picker";
 
 const { width, height } = Dimensions.get("screen");
@@ -52,43 +57,88 @@ const sections = [
 ];
 
 const Profile = ({ navigation }) => {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(
+    "http://172.20.10.4:4000/ProfileImage/profile.png"
+  );
+  const [modalVisible, setModalVisible] = useState(false);
 
-  const selectImageFromLibrary = async () => {
-    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+  const pickImage = async (source) => {
+    try {
+      let result;
+      if (source === "camera") {
+        const { status } = await ImagePicker.requestCameraPermissionsAsync();
+        if (status !== "granted") {
+          Alert.alert("Access to camera is required");
+        }
+        result = await ImagePicker.launchCameraAsync({
+          allowsEditing: true,
+          aspect: [1, 1],
+          quality: 1,
+        });
+      } else {
+        const {
+          status,
+        } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== "granted") {
+          Alert.alert("Access to camera roll is required");
+        }
 
-    if(status !== "granted") {
-      alert("Access to camera roll is required");
-      return;
-    
-  } else {
-    let result = await ImagePicker.launchCameraAsync({
-      allowsEditing: true,
-      aspect: [1, 1],
-      quality: 1,
-    });
+        result = await ImagePicker.launchImageLibraryAsync({
+          allowsEditing: true,
+          aspect: [1, 1],
+          quality: 1,
+        });
+      }
 
-    if (!result.canceled) {
-      setSelectedImage(result.assets[0].uri);
-    } else {
-      console.log("Canceled!");
+      if (!result.canceled) {
+        setSelectedImage(result.assets[0].uri);
+        setModalVisible(false);
+      } else {
+        Alert.alert("Canceled!");
+      }
+    } catch (error) {
+      Alert.alert(error.message);
     }
-  }
-  
-};
+  };
 
-  // const result = await ImagePicker.launchImageLibraryAsync({
-  //   allowsEditing: false,
-  //   aspectRatio: 1,
-  //   quality: 0.5,
-  // })
+  const RenderOptions = ({ title, image, action }) => {
+    return (
+      <TouchableOpacity
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+        onPress={() => action()}
+      >
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "500",
+            color: COLORS.mainColor,
+          }}
+        >
+          {title}
+        </Text>
+        <Image
+          source={image}
+          style={{
+            width: 30,
+            height: 30,
+            alignSelf: "center",
+            tintColor: COLORS.mainColor,
+          }}
+        />
+      </TouchableOpacity>
+    );
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.profile}>
         <TouchableOpacity
           style={styles.imageWrapper}
-          onPress={() => selectImageFromLibrary()}
+          onPress={() => setModalVisible(true)}
         >
           <Image
             resizeMode="contain"
@@ -147,7 +197,7 @@ const Profile = ({ navigation }) => {
           <TouchableOpacity>
             <Text
               style={{
-                fontSize: 16,
+                fontSize: 18,
                 fontWeight: "500",
                 color: "#F55F1F",
               }}
@@ -157,6 +207,77 @@ const Profile = ({ navigation }) => {
           </TouchableOpacity>
         </View>
       </ScrollView>
+
+      <Modal animationType="slide" transparent={true} visible={modalVisible}>
+        <View
+          style={{
+            width,
+            height: 300,
+            backgroundColor: COLORS.mainColor,
+            position: "absolute",
+            bottom: 0,
+            justifyContent: "center",
+            alignItems: "center",
+            borderTopLeftRadius: 30,
+            borderTopRightRadius: 30,
+            paddingHorizontal: 16,
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => setModalVisible(false)}
+            style={{
+              position: "absolute",
+              top: 20,
+              right: 10,
+              width: 40,
+              height: 40,
+              borderRadius: 20,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              source={remove}
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: COLORS.white,
+              }}
+            />
+          </TouchableOpacity>
+
+          <View
+            style={{
+              width: "100%",
+              height: "auto",
+              backgroundColor: COLORS.white,
+              paddingHorizontal: 16,
+              paddingVertical: 32,
+              marginTop: 50,
+              borderRadius: 30,
+            }}
+          >
+            <RenderOptions
+              title="Tacke photo"
+              image={camera}
+              action={() => pickImage("camera")}
+            />
+            <View
+              style={{
+                width: "100%",
+                height: 1,
+                backgroundColor: "#DDD",
+                marginVertical: 16,
+              }}
+            ></View>
+            <RenderOptions
+              title="Choose photo"
+              image={gallery}
+              action={() => pickImage("gallery")}
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
